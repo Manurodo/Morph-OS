8fabe17b671b05901ba5ee0a14d3ddad952ed865
Manurodo <manuelrosales2005@outlook.es>
2026-09-04 15:36:40 +0200

    mp3 oog

--- Morph/conv_/MP3_OOG.py
+++ Morph/conv_/MP3_OOG.py
@@ -1,7 +1,13 @@
 import os
+from pathlib import Path
 from tkinter import filedialog, ttk
-from anyio import Path
+from panel import extension
 from pydub import AudioSegment
+import threading
+import sys
+from pathlib import Path
+
+
 class conv_mp3_ogg:
     nombre = "MP3 → OGG"
     
@@ -13,88 +19,136 @@ class conv_mp3_ogg:
     def get_frame(self, parent):
         frame = ttk.Frame(parent)
 
-        # Boton para seleccionar carpeta entrada
-        boton_carpeta_in = ttk.Button(
+        self.root = parent.winfo_toplevel()
+
+        button_dir_in = ttk.Button(
             frame,
-            text="Seleccionar Carpeta Entrada",
+            text="Seleccionar Carpeta MP3",
             command=self.select_directory_in
         )
-        boton_carpeta_in.grid(row=0, column=0, columnspan=2, pady=5)
+        button_dir_in.grid(row=0, column=0, columnspan=2, pady=5)
 
-        #Boton para seleccionar carpeta salida
-        boton_carpeta_out = ttk.Button(
-                    frame,
-                    text="Seleccionar Carpeta Salida",
-                    command=self.select_directory_out
-                )
-        boton_carpeta_out.grid(row=1, column=0, columnspan=2, pady=5)
+        if self.dir_in:
+            self.entry_dir.insert(0, self.dir_in)
 
-        #Boton convertir
-        boton = ttk.Button(
+        button_dir_out = ttk.Button(
+            frame,
+            text="Seleccionar Carpeta OGG",
+            command=self.select_directory_out
+        )
+        button_dir_out.grid(row=1, column=0, columnspan=2, pady=5)
+
+        if self.dir_out:
+            self.entry_dir_out.insert(0, self.dir_out)
+
+
+        self.button_convert = ttk.Button(
             frame, 
             text="Convertir", 
             command=self.convertir
-        )
-        boton.grid(row=2, column=0, columnspan=2, pady=5)
+            )
+        self.button_convert.grid(row=2, column=0, columnspan=2, pady=5)
 
-        # Resultado
-        self.label_result = ttk.Label(frame, text="")
-        self.label_result.grid(
+        self.progress = ttk.Progressbar(
+            frame,
+            orient="horizontal",
+            length=250,
+            mode="determinate"
+        )
+        self.progress.grid(
             row=3,
             column=0,
             columnspan=2,
             padx=5,
             pady=5
         )
+
+        self.label_result = ttk.Label(frame, text="")
+        self.label_result.grid(
+            row=4, 
+            column=0, 
+            columnspan=2, 
+            padx=5, 
+            pady=5
+        )
+
         return frame
 
     def select_directory_in(self):
         ruta_carpeta = filedialog.askdirectory(
-            title="Selecciona una carpeta"
+            title="Selecciona una carpeta de entrada"
         )
 
         if ruta_carpeta:
             self.dir_in = Path(ruta_carpeta)
 
-            print("Ruta seleccionada:")
-            print(self.dir_in)
-
             self.label_result.config(
-                text=f"Carpeta: {self.dir_in}"
+                text=f"Entrada: {self.dir_in}"
             )
-
         else:
             print("No se seleccionó ninguna carpeta.")
-    
+
     def select_directory_out(self):
         ruta_carpeta = filedialog.askdirectory(
-            title="Selecciona una carpeta"
+            title="Selecciona una carpeta de salida"
         )
 
         if ruta_carpeta:
             self.dir_out = Path(ruta_carpeta)
 
-            print("Ruta seleccionada:")
-            print(self.dir_out)
-
             self.label_result.config(
-                text=f"Carpeta: {self.dir_out}"
+                text=f"Salida: {self.dir_out}"
             )
-
         else:
             print("No se seleccionó ninguna carpeta.")
 
+    
     def convertir(self):
-        dir_in = self.entry_dir.get().strip() or "."
-        dir_out = self.entry_dir_out.get().strip() or None
+        dir_in = self.dir_in
+        dir_out = self.dir_out
         try:
             cambios = self.mp3_ogg(dir_in, dir_out, self.bitrate)
             self.label_result.config(text=cambios)
         except Exception as e:
             self.label_result.config(text=f"Error: {e}")
 
+        # Desactivar botón mientras convierte
+        self.button_convert.config(state="disabled")
+
+        self.label_result.config(
+            text="Iniciando conversión..."
+        )
+
+        # Crear hilo
+        hilo = threading.Thread(
+            target=self.mp3_ogg,
+            args=(self.dir_in, self.dir_out, self.bitrate),
+            daemon=True
+        )
+
+        hilo.start()
+
+    def get_ffmpeg_path(self):
+    
+        if getattr(sys, "frozen", False):
+            # Estamos ejecutando el .exe
+            base_path = Path(sys._MEIPASS)
+        else:
+            # Estamos ejecutando desde Python
+            base_path = Path(__file__).resolve().parent.parent
+
+        ffmpeg_path = (
+            base_path
+            / "bin"
+            / "ffmpeg"
+            / "bin"
+        )
+
+        return ffmpeg_path
+
     def mp3_ogg(self, dir_in, dir_out=None, bitrate="192k"):
-            
+
+        try:    
             if not os.path.isdir(dir_in):
                 raise NotADirectoryError(f"No existe la carpeta: {dir_in}")
 
@@ -104,6 +158,21 @@ class conv_mp3_ogg:
 
             # Contador de conversiones
             contador = 0
+
+            # Contador de archivos MP3
+            ruta_carpeta = Path(dir_in)
+            extension = "*.mp3"  
+
+            cantidad = len(list(ruta_carpeta.glob(extension)))
+
+            self.root.after(
+                0,
+                lambda: self.progress.config(
+                    maximum=cantidad,
+                    value=0
+                )
+            )
+            
 
             for archivo in os.listdir(dir_in):
                 if archivo.lower().endswith(".mp3"):
@@ -116,8 +185,47 @@ class conv_mp3_ogg:
                         audio.export(ruta_salida, format="ogg", bitrate=bitrate)
                         print(f"✅ Convertido: {archivo} → {nombre_salida}")
                         contador += 1
+                        self.root.after(
+                            0,
+                            self.actualizar_progreso,
+                            contador,
+                            cantidad   
+                        )
                     except Exception as e:
                         print(f"❌ Error al convertir {archivo}: {e}")
 
+            self.root.after(
+                0,
+                self.conversion_terminada,
+                cantidad
+            )
             resultado = f"\n🎵 Conversión completa: {contador} archivos convertidos en '{dir_out}'"
             return resultado
+        
+        except Exception as e:
+            print("ERROR:")
+            print(e)
+            self.root.after(
+                0,
+                self.error_conversion,
+                str(e)
+            )
+
+    def actualizar_progreso(self, valor, total):
+        self.progress.config(value=valor)
+        self.label_result.config(
+            text=f"Progreso: {valor}/{total} archivos convertidos"
+        )
+
+    def conversion_terminada(self, total):
+        self.progress.config(value=total)
+        self.label_result.config(
+            text=f"Conversión terminada: {total} archivos convertidos"
+        )
+        self.button_convert.config(state="normal")
+
+    def error_conversion(self, error):
+        self.label_result.config(
+            text=f"Error: {error}"
+        )
+        self.button_convert.config(state="normal")
